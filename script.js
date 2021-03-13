b00d3c68b812db28cfc5fcc81fe96f45eb3c7789
Thomas Geer <thomas.g.musler@gmail.com>
2021-03-13 11:45:41 -0500

    attempting to retain main

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-var instance = M.FormSelect.getInstance(elem);
+var btnEl = document.querySelector("#btn");
+var inputEl = document.querySelector("#keyword");
 
+var key = "86a96f87ec4dbad68a9ea4356c58fe4a";
+
+btnEl.addEventListener("click", function(event) {
+    event.preventDefault();;
+
+    var keyword = inputEl.value;
+    var s = "http://api.mediastack.com/v1/news?access_key=" + key + "&keywords=" + keyword;
+
+    fetch(s,  {
+    })
+    .then(function (data) {
+        console.log(data);
+});
 
 document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('select');
-  var instances = M.FormSelect.init(elems, options);
-});
-instance.getSelectedValues();
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+  });
+
