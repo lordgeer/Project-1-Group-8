464f00d7d328bb4f6f0bbbc02ff74fabf384216a
Broc C <bac5806@gmail.com>
2021-03-13 14:12:21 -0500

    pull requests from api working, some data on page

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,19 @@
 var btnEl = document.querySelector("#btn");
 var inputEl = document.querySelector("#keyword");
+var cardEl = document.querySelector(".row");
+var cardTitleEl = document.querySelector(".card-title");
+var artDescrEl = document.querySelector(".articleDescr");
+var linkEl = document.querySelector("#link");
 
 var key = "86a96f87ec4dbad68a9ea4356c58fe4a";
+
+var info;
 
 btnEl.addEventListener("click", function(event) {
     event.preventDefault();;
 
     var keyword = inputEl.value;
-    var s = "http://api.mediastack.com/v1/news?access_key=" + key + "&keywords=" + keyword;
+    var s = "http://api.mediastack.com/v1/news?access_key=" + key + "&keywords=" + keyword + "&languages=en";
 
     fetch(s,  {
     })
@@ -15,7 +21,14 @@ btnEl.addEventListener("click", function(event) {
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
+        console.log(data);   
+
+        cardEl.style.display = "block";
+
+        cardTitleEl.innerHTML = data.data[24].title;
+        artDescrEl.innerHTML = data.data[24].description;
+        linkEl.textContent = "Link to Article";
+        linkEl.href = data.data[24].url;  
     });
 });
 
@@ -23,6 +36,5 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var options = document.querySelector("#opt1");
     var instances = M.FormSelect.init(elems, options);
-
-
   });
+
