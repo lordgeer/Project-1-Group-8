d59ebf46ef2b30c01a1bff2fb7be8ba36a66c2b8
Thomas G Musler <thomas.g.musler@gmail.com>
2021-03-15 19:55:32 -0400

    event listener for selection modal

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 var btnEl = document.querySelector("#btn");
 var inputEl = document.querySelector("#keyword");
-var bodyEl = document.querySelector("body");
+var cardEl = document.querySelector(".row");
+var cardTitleEl = document.querySelector(".card-title");
+var artDescrEl = document.querySelector(".articleDescr");
+var linkEl = document.querySelector("#link");
 
 var key = "86a96f87ec4dbad68a9ea4356c58fe4a";
 var guardianApi = `9d659950-2409-48a5-b628-08c9ecdb8d45`
@@ -12,7 +15,7 @@ btnEl.addEventListener("click", function(event) {
     event.preventDefault();;
 
     var keyword = inputEl.value;
-    var s = "http://api.mediastack.com/v1/news?access_key=" + key + "&keywords=" + keyword + "&languages=en" + "&sources = " + ;
+    var s = "http://api.mediastack.com/v1/news?access_key=" + key + "&keywords=" + keyword + "&languages=en";
 
     fetch(s,  {
     })
@@ -21,37 +24,13 @@ btnEl.addEventListener("click", function(event) {
     })
     .then(function (data) {
         console.log(data);   
-        for (var i = 0; i < 10; ++i) {
-          var row = document.createElement("div");
-          row.className = "row";
-          var col = document.createElement("div");
-          col.className = "col s12 m6";
-          var card1El = document.createElement("div");
-          card1El.className = "card blue-grey darken-1";
-          var card2El = document.createElement("div");
-          card2El.className = "card-content white-text";
 
-          var spanEl = document.createElement("span");
-          spanEl.className = "card-title";
-          spanEl.innerHTML = data.data[i].title;
+        cardEl.style.display = "block";
 
-          var sourceEl = document.createElement("h6");
-          sourceEl.textContent = data.data[i].source;
-
-          var artDescr = document.createElement("p");
-          artDescr.innerHTML = data.data[i].description;
-          
-          var linkEl = document.createElement("a");
-          linkEl.textContent = "Link to Article";
-          linkEl.href = data.data[i].url;
-          linkEl.target = "_blank";  
-
-          card2El.append(spanEl, sourceEl, artDescr, linkEl);
-          card1El.append(card2El);
-          col.append(card1El);
-          row.append(col);
-          bodyEl.append(row);
-        }
+        cardTitleEl.innerHTML = data.data[24].title;
+        artDescrEl.innerHTML = data.data[24].description;
+        linkEl.textContent = "Link to Article";
+        linkEl.href = data.data[24].url;  
     });
 });
 
@@ -87,3 +66,12 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   };
 
+  document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelector('select');
+    elems.onchange = selectThem;
+    var instances = M.FormSelect.init(elems);
+    function selectThem() {
+        var selectedOne = instances.getSelectedValues();
+        console.log(selectedOne);
+    }
+});
