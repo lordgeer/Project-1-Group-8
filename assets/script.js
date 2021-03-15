8d0f66539487b0d67c554322839e93c93b2a5333
Broc C <bac5806@gmail.com>
2021-03-15 19:58:43 -0400

    format

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,8 @@ var btnEl = document.querySelector("#btn");
 var inputEl = document.querySelector("#keyword");
 var bodyEl = document.querySelector("body");
 
+// mediastack api key
 var key = "86a96f87ec4dbad68a9ea4356c58fe4a";
-
-var info;
 
 btnEl.addEventListener("click", function(event) {
     event.preventDefault();;
@@ -53,35 +52,18 @@ btnEl.addEventListener("click", function(event) {
     });
 });
 
-
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var options = document.querySelector("#opt1");
     var instances = M.FormSelect.init(elems, options);
   });
 
-  var displayResults = function (keyword, searchTerm) {
-    
-    searchTerm.textContent = searchTerm;
-  
-    for (var i = 0; i < keyword.length; i++) {
-      var sourceName = keyword[i].owner.login + '/' + keyword[i].name;
-  
-      var repoEl = document.createElement('a');
-      repoEl.classList = 'list-item flex-row justify-space-between align-center';
-      repoEl.setAttribute(keywordText + sourceName);
-  
-      var titleEl = document.createElement('span');
-      titleEl.textContent = sourceName;
-  
-      repoEl.appendChild(titleEl);
-  
-      var statusEl = document.createElement('span');
-      statusEl.classList = 'flex-row align-center';
-  
-      repoEl.appendChild(statusEl);
-  
-      repoContainerEl.appendChild(repoEl);
+  document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelector('select');
+    elems.onchange = selectThem;
+    var instances = M.FormSelect.init(elems);
+    function selectThem() {
+        var selectedOne = instances.getSelectedValues();
+        console.log(selectedOne);
     }
-  };
-
+});
