a75a279e9a8dd8c82b3644d302b0ae5811f59d40
Broc C <bac5806@gmail.com>
2021-03-18 13:59:58 -0400

    added logic to avoid displaying duplicate articles to the page

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,7 @@ var instances = M.FormSelect.init(elems);
 var cardContainer = document.createElement("div");
 
 // mediastack api key
-var key = "86a96f87ec4dbad68a9ea4356c58fe4a";
+var key = "e719427a4fbbb202e3d0acd1d55f1a41";
 
 // guardian api key
 var guardianApi = "9d659950-2409-48a5-b628-08c9ecdb8d45";
@@ -15,7 +15,8 @@ var guardianApi = "9d659950-2409-48a5-b628-08c9ecdb8d45";
 // array to store user options from modal
 var storeSources = [];
 
-var test = [];
+// array to store article titles to check for duplicates
+var storeTitle = [];
 
 // event listener for user selected sources from the modal
 document.addEventListener('DOMContentLoaded', function () {
@@ -43,7 +44,6 @@ btnEl.addEventListener("click", function(event) {
 
     // create an empty string used for creating api url
     var apiUrl = "";
-    console.log("length: " + hasSelected.length + "\nsel: " + hasSelected);
     // check if the user did not select any sources from the modal, fetch from guardian api
     if ((hasSelected.length == 1 && hasSelected == 0) || hasSelected.length == 0)
     {
@@ -83,8 +83,8 @@ btnEl.addEventListener("click", function(event) {
             // create span element for displaying article title 
             var spanEl = document.createElement("span");
             spanEl.className = "card-title";
-            spanEl.innerHTML = data.response.results[i].webTitle;
-
+            spanEl.textContent = data.response.results[i].webTitle;
+            
             // create H6 element for displaying article type/genre
             var typeEl = document.createElement("h6");
             typeEl.textContent = "Article Type: " + data.response.results[i].sectionName;
@@ -106,6 +106,7 @@ btnEl.addEventListener("click", function(event) {
             cardContainer.append(row);
           }
       });
+
     }
     else 
     {
@@ -123,15 +124,14 @@ btnEl.addEventListener("click", function(event) {
         } 
       }
 
-      console.log(storeSources);
-
       // store array of selected sources in local storage
       localStorage.setItem("newsSources", JSON.stringify(storeSources));
       // if user selects source from modal, set api url to mediastack
       var storeSourcesStr = storeSources.join(",");
-      console.log("str1: " + storeSourcesStr);
+
+      //  set api url
       apiUrl = "http://api.mediastack.com/v1/news?access_key=" + key + "&keywords=" + keyword + "&sources=" + storeSourcesStr + "&limit=25" + "&languages=en";
-      console.log("str2: " + apiUrl);
+
       // fetch api url
       fetch(apiUrl,  {
       })
@@ -139,8 +139,28 @@ btnEl.addEventListener("click", function(event) {
           return response.json();
       })
       .then(function (data) {
-        console.log(data);
-          for (var i = 0; i < 10; ++i) {
+            console.log(data);
+
+            for (var i = 0; i < data.data.length; ++i) {
+            
+            // boolean variable
+            var isSame = false;
+            // loop through stores article titles
+            for (var j = 0; j < storeTitle.length; ++j) {
+              if (data.data[i].title == test[j]) {
+                // if article title matches one that's 
+                // already stored, set the flag to true
+                isSame = true;
+                break;
+              }
+            }
+
+            // if isSame flag is set to true, 
+            // continue to next iteration
+            if (isSame) {
+              continue;
+            }
+
             // create dynamic html elements
             // create div and set its class
             var row = document.createElement("div");
@@ -158,7 +178,10 @@ btnEl.addEventListener("click", function(event) {
             // create span element for displaying article title
             var spanEl = document.createElement("span");
             spanEl.className = "card-title";
-            spanEl.innerHTML = data.data[i].title;
+            spanEl.textContent = data.data[i].title;
+
+            // if article is unique, store in array
+            storeTitle.push(data.data[i].title);
 
             // create H6 element for displaying article type/genre
             var sourceEl = document.createElement("h6");
@@ -199,14 +222,3 @@ function init() {
 }
 
 init();
-
-
-//   document.addEventListener('DOMContentLoaded', function () {
-//     var elems = document.querySelector('select');
-//     elems.onchange = selectThem;
-//     var instances = M.FormSelect.init(elems);
-//     function selectThem() {
-//         var selectedOne = instances.getSelectedValues();
-//         console.log(selectedOne);
-//     }
-// });
